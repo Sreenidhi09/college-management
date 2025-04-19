37dfc4e28be4c058f2e690e5efde83f278fece6c
Sreenidhi09 <sreenidhi02092006@gmail.com>
2025-04-19 15:10:11 +0530

    Fix bcrypt issue for Render

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
